feb7f9dbc3da6584ab7856dadd788a10a57d6937
Ben Ohling <b.ohling@outlook.com>
2026-06-07 12:06:56 -0700

    chore: expose modules in __init__.py

--- wordprobe/__init__.py
+++ wordprobe/__init__.py
@@ -1,4 +1,11 @@
 """Core package modules for the Wordler solver rewrite."""
+from .abstract import *
+from .core import *
+from .constraints import *
+from .db import *
+from .heuristics import *
+from .models import *
+
 __version__ = "0.2.0"
 __author__ = "Ben Ohling"
 __copyright__ = f"© 2026 {__author__}"
